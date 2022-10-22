0dfe90a0b1e6d19c312e504026398ffe029119da
Eduardo Adams <eduardoadams2307@gmail.com>
2022-10-22 00:43:14 -0300

    atualizacion

--- js/moeda.js
+++ js/moeda.js
@@ -111,6 +111,28 @@ am5.ready(function() {
     
     }); // end am5.ready()
 
-___________________________________________________________________________________________
+///////////////////////////////////////////////////////////////////////////////////////
 
+
+//ANIMAÇÃO
+
+const target = document.querySelectorAll('[data-anime]')
+const animacao = 'animacao'
+
+function animeSite(){
+  const windonTop = window.pageYOffset + ((window.innerHeight *3)/4)
+  target.forEach((elemento)=>{
+    if((windonTop)>elemento.offsetTop){
+      elemento.classList.add('animacao')
+    }else{
+      elemento.classList.remove('animacao')
+    }
+
+    console.log(elemento.offsetTop)
+  })
+}
+
+window.addEventListener('scroll', ()=>{
+  animeSite()
+})
 
