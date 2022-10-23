53e168e199d0b697a5457c71b9108ee9c7c43234
Eduardo Adams <eduardoadams2307@gmail.com>
2022-10-23 00:31:27 -0300

    atualizacion

---
+++ js/index.js
@@ -0,0 +1,21 @@
+//ANIMAÇÃO SCROLL
+
+const target = document.querySelectorAll('[data-anime]')
+const animacao = 'animacao'
+
+function animeSite(){
+  const windonTop = window.pageYOffset + ((window.innerHeight *3)/4)
+  target.forEach((elemento)=>{
+    if((windonTop)>elemento.offsetTop){
+      elemento.classList.add('animacao')
+    }else{
+      elemento.classList.remove('animacao')
+    }
+
+    console.log(elemento.offsetTop)
+  })
+}
+
+window.addEventListener('scroll', ()=>{
+  animeSite()
+})
